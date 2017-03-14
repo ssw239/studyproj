ea9ab623885bd6db7d03711391708bb22c6b4fbe
nebsdsamwu <nebsd.sam.wu@gmail.com>
2017-03-13 23:01:54 -0700

    Add project files.

---
+++ src/Ch5Bit/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ch5Bit
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Insertion(1024, 76, 1, 1);
+            Console.ReadKey();
+        }
+
+        static void Insertion (int n, int m, int i, int j)
+        {
+            Console.WriteLine(Convert.ToString(n, 2).PadLeft(11, '0'));
+            Console.WriteLine(Convert.ToString(m, 2).PadLeft(11, '0'));
+        }
+    }
+}

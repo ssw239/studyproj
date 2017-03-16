b1c5cd4686601be0af3a459b581916bf4473c5f4
nebsdsamwu <nebsd.sam.wu@gmail.com>
2017-03-16 00:25:14 -0700

    5.2 done

--- src/Ch5Bit/Program.cs
+++ src/Ch5Bit/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text;
 
 namespace Ch5Bit
 {
@@ -9,7 +10,8 @@ namespace Ch5Bit
     {
         public static void Main(string[] args)
         {
-            Insertion(1024, 19, 2, 6);
+            // Insertion(1024, 19, 2, 6);
+            RealToBinary(0.625);
             Console.ReadKey();
         }
 
@@ -18,7 +20,43 @@ namespace Ch5Bit
             Console.WriteLine(Convert.ToString(n, 2).PadLeft(11, '0'));
             Console.WriteLine(Convert.ToString(m, 2).PadLeft(11, '0'));
             Console.WriteLine(Convert.ToString((m <<= i) | n, 2).PadLeft(11, '0'));
+            
+        }
 
+        static int RealToBinary(double r)
+        {
+            if (r >= 1 || r <= 0)
+            {
+                Console.WriteLine("Invalid Number");
+                return 0;
+            }
+
+            StringBuilder binStr = new StringBuilder();
+            while(r > 0)
+            {
+                if (binStr.Length > 32)
+                {
+                    Console.WriteLine("Invalid Number");
+                    return 0;
+                }
+
+                double t = r * 2;
+
+                if (t >= 1)
+                {
+                    binStr.Append("1");
+                    r = t - 1;
+                }
+                else
+                {
+                    binStr.Append("0");
+                    r = t;
+                }
+
+                Console.WriteLine(r.ToString() + ":" + binStr.ToString());
+            }
+
+            return 1;
         }
     }
 }

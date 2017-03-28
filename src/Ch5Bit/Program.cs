c873fa1ead92dbf1563b97385efd393eabee10f1
nebsdsamwu <nebsd.sam.wu@gmail.com>
2017-03-27 22:26:53 -0700

    5.2 flip

--- src/Ch5Bit/Program.cs
+++ src/Ch5Bit/Program.cs
@@ -11,8 +11,59 @@ namespace Ch5Bit
         public static void Main(string[] args)
         {
             // Insertion(1024, 19, 2, 6);
-            RealToBinary(0.625);
+            // RealToBinary(0.625);
+            FlipToWin(17753);
             Console.ReadKey();
+        }
+
+        // 5.3
+        static void FlipToWin(int n)
+        {
+            StringBuilder s = new StringBuilder();
+            string bstr = Convert.ToString(n, 2);
+            char[] ba = bstr.ToArray();
+            Console.WriteLine(Convert.ToString(n, 2));
+            int idx1 = 0;
+            bool flipped = false;
+            int len1 = 0;
+            int len2 = 0;
+            int fOnes = 0;
+            for(int i = 0; i < ba.Length; i++)
+            {
+                Console.WriteLine(ba[i]);
+                if (ba[i] == '1')
+                {
+                    len1 += 1;
+                    if (flipped)
+                    {
+                        fOnes += 1;
+                    }
+                }
+                else if (ba[i] == '0')
+                {
+                    if (! flipped)
+                    {
+                        len1 += 1;
+                        flipped = true;
+                    }
+                    else if (flipped)
+                    {
+                        if (len1 > len2)
+                        {
+                            len2 = len1;
+                        }
+                        len1 = fOnes + 1;
+                        fOnes = 0;
+                    }
+                }
+            }
+
+            if (len1 > len2)
+            {
+                len2 = len1;
+            }
+            Console.WriteLine("result:" + len1);
+            Console.WriteLine("result:" + len2);
         }
 
         static void Insertion(int n, int m, int i, int j)
@@ -32,6 +83,7 @@ namespace Ch5Bit
             }
 
             StringBuilder binStr = new StringBuilder();
+            binStr.Append(".");
             while(r > 0)
             {
                 if (binStr.Length > 32)
